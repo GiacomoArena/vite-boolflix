547ad93420fb9ebc7e0f8fa5df0f376270ea1743
GiacomoArena <giacomoarena28@gmail.com>
2023-05-05 11:30:50 +0200

    make card overview and animation

--- src/components/partials/MovieCard.vue
+++ src/components/partials/MovieCard.vue
@@ -11,6 +11,7 @@
         store,
         vote: this.card.vote_average,
         voteString:'',
+        zeroStars:'<i class="fa-regular fa-star"></i>',
       }
     },
     methods:{
@@ -38,10 +39,12 @@
     <img class="card-img" v-else :src="`https://image.tmdb.org/t/p/w500/${card.backdrop_path}`
 " alt="card">
     <div class="card-info ">
-      <h5>{{card.title}}</h5>
+      <h5 class="titles" >{{card.title}}</h5>
 
-      <h5 v-if="card.original_title === card.title"></h5>
-      <h5 v-else>{{card.original_title}}</h5>
+      <h5  class="titles" v-if="card.original_title === card.title">&nbsp</h5>
+      <h6  class="titles" v-else>{{card.original_title}}</h6>
+
+      <span class="overview">{{ card.overview }}</span>
 
       <span v-if="card.original_language === 'en'"> <img class="flag" src="../../assets/img/en.png" alt="flag"></span>
       <span v-if="card.original_language === 'it'"> <img class="flag" src="../../assets/img/it.png" alt="flag"></span>
@@ -52,8 +55,9 @@
 
       <span v-else>  {{card.original_language}}   </span>
 
+      <div v-if="voteString <=  1" class="stars"  v-html="zeroStars"></div>
 
-      <div class="stars"  v-html="voteString"></div>
+      <div v-else class="stars"  v-html="voteString"></div>
     </div>
   </div>
 
@@ -63,13 +67,75 @@
 <style lang="scss" scoped>
 
 .card-container{
+  position: relative;
   display: flex;
   flex-direction: column;
   margin: 20px;
+  .overview{
+    display: none;
+  }
+  &:hover .card-info{
+    display: block;
+    font-size: 15px;
+    width: 100%;
+    height: 100%;
+    transform: scale(1.5);
+    transition: all 1s;
+    position: absolute;
+    background-color: brown;
+    //bottom: -90px;
+    right: 0;
+    z-index: 5;
+    h5{
+      font-size: 15px;
+    }
+    .titles{
+      width: 50%;
+    }
+    h6{
+      font-size: 10px;
+    }
+    .overview{
+      display: block;
+      position: absolute;
+      top: 0px;
+      z-index: 455;
+      width: 130px;
+      height: 130px;
+      overflow-y: auto;
+      right: 0;
+  }
+  }
+  .card-info{
+    display: flex;
+    font-size: 1px;
+    width: 10px;
+    right: 50%;
+    position: absolute;
+    background-color: brown;
+    z-index: 0;
+    h5{
+      transition: all 0.5s;
+      font-size: 1px;
+    }
+  }
+  
   .card-img{
+    position: relative;
+    transition: all 1s;
+    cursor: pointer;
     width: 300px;
     height: 150px;
+    z-index: 3;
+    &:hover{
+      transform: scale(1.5);
+      z-index: 4;
+    }
+    
   }
+  
+  
+  
   .flag{
     width: 40px;
     margin: 5px;
