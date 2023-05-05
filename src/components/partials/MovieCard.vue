3700376f431ad6e6c05d497cc0bd74670ac7ac85
GiacomoArena <giacomoarena28@gmail.com>
2023-05-05 15:53:44 +0200

    update commit

--- src/components/partials/MovieCard.vue
+++ src/components/partials/MovieCard.vue
@@ -12,9 +12,16 @@
         vote: this.card.vote_average,
         voteString:'',
         zeroStars:'<i class="fa-regular fa-star"></i>',
+        flag:false,
       }
     },
     methods:{
+      flagChange(){
+        this.flag = !this.flag
+      },
+      flagChangeFalse(){
+        this.flag = false
+      },
       getStars(){
         let icon = '<i class="fa-solid fa-star"></i> '
         let votes = Math.ceil(this.vote) /2
@@ -29,22 +36,22 @@
     }
   }
 
-  // ${card.results.backdrop_path}
   </script>
 
 <template>
   
-  <div class="card-container">
-    <img class="card-img" v-if="card.backdrop_path === null" src="../../assets/img/no_images.jpg" alt="card">
-    <img class="card-img" v-else :src="`https://image.tmdb.org/t/p/w500/${card.backdrop_path}`
+  <div class="card-container" @mouseleave="flagChangeFalse()">
+    <img @click="flagChange()"   class="card-img" v-if="card.backdrop_path === null" src="../../assets/img/no_images.jpg" alt="card">
+    <img @click="flagChange()" class="card-img" v-else :src="`https://image.tmdb.org/t/p/w500/${card.backdrop_path}`
 " alt="card">
-    <div class="card-info ">
+    <span class="info-text"><i class="fa-solid fa-info"></i> </span>
+    <div :class="{'card-info ' : flag === true}" @click="flagChange()" class="d-none">
       <h5 class="titles" >{{card.title}}</h5>
 
-      <h5  class="titles" v-if="card.original_title === card.title">&nbsp</h5>
+      <h5  class="titles" v-if="card.original_title === card.title">&nbsp;</h5>
       <h6  class="titles" v-else>{{card.original_title}}</h6>
 
-      <span class="overview">{{ card.overview }}</span>
+      <span class="overview"><h3>Description</h3>{{ card.overview }}</span>
 
       <span v-if="card.original_language === 'en'"> <img class="flag" src="../../assets/img/en.png" alt="flag"></span>
       <span v-if="card.original_language === 'it'"> <img class="flag" src="../../assets/img/it.png" alt="flag"></span>
@@ -65,7 +72,7 @@
 
 
 <style lang="scss" scoped>
-
+  @import '../../scss/partials/variables';
 .card-container{
   position: relative;
   display: flex;
@@ -74,50 +81,75 @@
   .overview{
     display: none;
   }
-  &:hover .card-info{
-    display: block;
+  .info-text{
+    opacity: 0;
+    font-size: 1px;
+  }
+ /* //da sistemare
+  .card-container:hover .info-text{
+    opacity: 1;
+    font-size: 16px;
+    width: 100px;
+    height: 100px;
+    text-align: center;
+    position: absolute;
+    bottom: 25px;
+    z-index: 99999;
+    background-color: rgba(168, 161, 161, 0.493);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    right: 100px;
+    border-radius: 50%;
+    cursor: pointer;
+  }
+  //da sistemare*/
+  .card-info{
+    overflow-y:auto ;
+    -webkit-box-shadow: 0px 0px 13px 4px #000000;
+    -moz-box-shadow: 0px 0px 13px 4px #000000;
+    -o-box-shadow: 0px 0px 13px 4px #000000;
+    box-shadow: 0px 0px 13px 4px #000000;
+    display: block !important;
     font-size: 15px;
     width: 100%;
     height: 100%;
     transform: scale(1.5);
     transition: all 1s;
     position: absolute;
-    background-color: brown;
-    //bottom: -90px;
+    background-color: rgba(34, 30, 30, 0.945);
+    cursor: pointer;
     right: 0;
     z-index: 5;
     h5{
+      margin-top: 3px;
       font-size: 15px;
     }
     .titles{
       width: 50%;
+      text-align: center;
     }
     h6{
       font-size: 10px;
+      color: $primary-color;
     }
     .overview{
       display: block;
       position: absolute;
-      top: 0px;
+      top: 5px;
+      font-weight: 400;
+      font-size: 12px;
       z-index: 455;
       width: 130px;
-      height: 130px;
+      height: 140px;
       overflow-y: auto;
       right: 0;
+      h3{
+        font-size: 18px;
+        color: $primary-color;
+        font-weight: 500;
+      }
   }
-  }
-  .card-info{
-    display: flex;
-    font-size: 1px;
-    width: 10px;
-    right: 50%;
-    position: absolute;
-    background-color: brown;
-    z-index: 0;
-    h5{
-      transition: all 0.5s;
-      font-size: 1px;
-    }
   }
   
   .card-img{
@@ -131,16 +163,16 @@
       transform: scale(1.5);
       z-index: 4;
     }
-    
   }
-  
-  
-  
   .flag{
     width: 40px;
     margin: 5px;
   }
-  
+  span{
+    img{
+      width: 25px !important;
+    }
+  }
 }
 
 </style>
